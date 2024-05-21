87bf7b5455b5f48c3498bfad2cf8c5b94c665023
Caleb Joseph <calebbjoseph@gmail.com>
2024-05-21 15:04:42 -0500

    deleting unwanted user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,10 +25,7 @@ class AdminController extends Controller
 
     public function index(): View
     {
-        $id = User::where('name', 'Victor Balogun')->first()->id;
-        Application::where('user_id', $id)->delete();
-        User::where('name', 'Victor Balogun')->delete();
-
+        dd(Application::all());
         return view('home');
     }
 

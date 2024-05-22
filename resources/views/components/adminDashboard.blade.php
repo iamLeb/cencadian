6ff08ffae0ead9c75241155925d8f9ddc13618f3
Caleb Joseph <calebbjoseph@gmail.com>
2024-05-22 15:52:20 -0500

    update dshboard looks

--- resources/views/components/adminDashboard.blade.php
+++ resources/views/components/adminDashboard.blade.php
@@ -36,7 +36,7 @@
             <!--end row-->
 
             <div class="row">
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-3 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
@@ -60,7 +60,30 @@
                     </div><!-- end card -->
                 </div><!-- end col -->
 
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">No. of Company Request</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ \App\Models\ServiceRequest::count() }}">{{ \App\Models\Application::count() }}</span></h4>
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-info rounded fs-3">
+                                        <i class="bx bx-layer"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+                <div class="col-xl-3 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
@@ -84,13 +107,13 @@
                     </div><!-- end card -->
                 </div><!-- end col -->
 
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-3 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="flex-grow-1 overflow-hidden">
-                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">No. of Application</p>
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">No. of Intern Application</p>
                                 </div>
                             </div>
                             <div class="d-flex align-items-end justify-content-between mt-4">

943f0b90c0e8fb9a26e676435b365351f2f86597
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-03 10:02:25 -0500

    updated contents

--- resources/views/admin/intern/show.blade.php
+++ resources/views/admin/intern/show.blade.php
@@ -99,52 +99,59 @@
                                 </div><!-- end card -->
 
                                 @if ($user->emergency)
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-3">Emergency Information</h5>
-                                        <div class="table-responsive">
-                                            <table class="table table-borderless mb-0">
-                                                <tbody>
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Name:</th>
-                                                    <td class="text-muted">{{ $user->emergency->name }}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Primary:</th>
-                                                    <td class="text-muted">{{ $user->emergency->pphone }}</td>
-                                                </tr>
-                                                @if ($user->emergency->sphone)
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Secondary:</th>
-                                                    <td class="text-muted">{{ $user->emergency->sphone }}</td>
-                                                </tr>
-                                                @endif
-                                                <tr>
-                                                    <th class="ps-0" scope="row">E-mail:</th>
-                                                    <td class="text-muted">{{ $user->emergency->email }}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Address:</th>
-                                                    <td class="text-muted">{{ $user->emergency->address }}
-                                                    </td>
-                                                </tr>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5 class="card-title mb-3">Emergency Information</h5>
+                                            <div class="table-responsive">
+                                                <table class="table table-borderless mb-0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Name:</th>
+                                                        <td class="text-muted">{{ $user->emergency->name }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Primary:</th>
+                                                        <td class="text-muted">{{ $user->emergency->pphone }}</td>
+                                                    </tr>
+                                                    @if ($user->emergency->sphone)
+                                                        <tr>
+                                                            <th class="ps-0" scope="row">Secondary:</th>
+                                                            <td class="text-muted">{{ $user->emergency->sphone }}</td>
+                                                        </tr>
+                                                    @endif
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">E-mail:</th>
+                                                        <td class="text-muted">{{ $user->emergency->email }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Address:</th>
+                                                        <td class="text-muted">{{ $user->emergency->address }}
+                                                        </td>
+                                                    </tr>
 
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Relationship:</th>
-                                                    <td class="text-muted">{{ $user->emergency->relationship }}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th class="ps-0" scope="row">Note:</th>
-                                                    <td class="text-muted">
-                                                        <textarea class="form-control border-0" style="resize: none;">{{ $user->emergency->note }}</textarea>
-                                                    </td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div><!-- end card -->
-                              @endif
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Relationship:</th>
+                                                        <td class="text-muted">{{ $user->emergency->relationship }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Note:</th>
+                                                        <td class="text-muted">
+                                                            <textarea class="form-control border-0" style="resize: none;">{{ $user->emergency->note }}</textarea>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                @endif
+
+
+                                @if ($user->clock)
+                                    @foreach($user->clock as $clock)
+                                        <p>{{ $clock->created_at }}</p>
+                                    @endforeach
+                                @endif
 
                                 <div class="card">
                                     <div class="card-body">

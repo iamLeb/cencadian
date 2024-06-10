b8a034802b1ffdb8dfa83cc7cb65da2962006918
Phelickz <55344513+Phelickz@users.noreply.github.com>
2024-06-10 10:58:40 -0500

    edit-references

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -20,11 +20,14 @@ class ApplicationController extends Controller
 
     public function store(Request $request)
     {
+        // log something
+
+        echo 'ApplicationController@store() called.';
 
         $request['address'] = $request->city . ', ' . $request->country . ', ' . $request->postalCode;
 
         if ($request->hasFile("resume")) {
-            $fileName = time().auth()->id() . ".pdf";
+            $fileName = time() . auth()->id() . ".pdf";
 
             $request->file("resume")->storeAs('resume/', $fileName, 's3');
 
@@ -73,7 +76,7 @@ class ApplicationController extends Controller
                 //generate a random OTP and check that an existing record with the same OTP does not exist
                 $otp = 0;
                 do {
-                    $otp=random_int(1,999999);
+                    $otp = random_int(1, 999999);
                     $existingRecord = InternReference::where('otp', $otp);
                 } while (!$existingRecord);
 
@@ -97,6 +100,87 @@ class ApplicationController extends Controller
             "gender" => $request->gender
         ]);
 
+        return redirect()->back()->with('success', "Your Application Has Been Submitted.");
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+
+        $application = Application::findOrFail($id);
+        $application->update([
+            "school" => $request->school,
+            "skills" => $request->skills,
+            "note" => $request->note,
+        ]);
+
+
+        // delete all references of the application
+        $application->reference()->delete();
+
+        $references = [
+            [
+                "application_id" => $application->id,
+                "name" => $request->reference1_name,
+                "org" => $request->reference1_org,
+                "relationship" => $request->reference1_relationship,
+                "phone" => $request->reference1_phone,
+                "email" => $request->reference1_email,
+                "prefContact" => $request->reference1_prefContact,
+            ],
+
+            [
+                "application_id" => $application->id,
+                "name" => $request->reference2_name,
+                "org" => $request->reference2_org,
+                "relationship" => $request->reference2_relationship,
+                "phone" => $request->reference2_phone,
+                "email" => $request->reference2_email,
+                "prefContact" => $request->reference2_prefContact,
+            ],
+
+            [
+                "application_id" => $application->id,
+                "name" => $request->reference3_name,
+                "org" => $request->reference3_org,
+                "relationship" => $request->reference3_relationship,
+                "phone" => $request->reference3_phone,
+                "email" => $request->reference3_email,
+                "prefContact" => $request->reference3_prefContact,
+            ],
+        ];
+
+        //create reference
+        foreach ($references as $reference) {
+
+            //generate a random OTP and check that an existing record with the same OTP does not exist
+            $otp = 0;
+            do {
+                $otp = random_int(1, 999999);
+                $existingRecord = InternReference::where('otp', $otp);
+            } while (!$existingRecord);
+
+            InternReference::create([
+                "application_id" => $reference['application_id'],
+                "name" => $reference['name'],
+                "org" => $reference['org'],
+                "relationship" => $reference['relationship'],
+                "phone" => $reference['phone'],
+                "email" => $reference['email'],
+                "prefContact" => $reference['prefContact'],
+                "otp" => $otp
+            ]);
+        }
+
+        auth()->user()->update([
+            "phone" => $request->phone,
+            "address" => $request->address,
+            "dob" => $request->dob,
+            "gender" => $request->gender
+        ]);
+
+        echo 'ApplicationController@store() called.';
         return redirect()->back()->with('success', "Your Application Has Been Submitted.");
     }
 }

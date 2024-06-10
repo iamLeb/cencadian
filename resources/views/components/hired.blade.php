10e8f709f63dee5bd84cc70df4ffbf3af8adc952
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-10 11:53:51 -0500

    added hired intern dashboard

--- resources/views/components/hired.blade.php
+++ resources/views/components/hired.blade.php
@@ -48,7 +48,7 @@
             <!--end row-->
 
             <div class="row">
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-6 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
@@ -72,7 +72,7 @@
                     </div><!-- end card -->
                 </div><!-- end col -->
 
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-6 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
@@ -96,7 +96,7 @@
                     </div><!-- end card -->
                 </div><!-- end col -->
 
-                <div class="col-xl-4 col-md-6">
+                <div class="col-xl-6 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
                         <div class="card-body">
